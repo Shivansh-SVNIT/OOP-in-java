75ddb2fc3f9b2d9ce4b3f3ab93f8f9a637329b79
Shivansh-SVNIT <ds992568@gmail.com>
2026-02-16 19:33:49 +0530

    Add files via upload

---
+++ OOPlab4/Matrix1.java
@@ -0,0 +1,31 @@
+
+class Function
+{
+    static void set(int value,int indexX,int indexY,int indexZ, int [] Array)//3x3x3
+    {
+        int index=indexX*3*3+indexY*3+indexZ;
+        Array[index]=value;
+    }
+
+    static int get(int indexX,int indexY,int indexZ, int [] Array)
+    {
+        return Array[indexX*3*3+indexY*3+indexZ];
+    }
+}
+
+public class Matrix1
+{
+    public static void main(String[] args)
+    {
+        int[] arr=new int[27];
+       
+        Function.set(10, 0, 0, 0, arr);
+        Function.set(25, 1, 1, 1, arr);
+        Function.set(50, 2, 2, 2, arr);
+
+       
+        System.out.println(Function.get(0, 0, 0, arr));
+        System.out.println(Function.get(1, 1, 1, arr));
+        System.out.println(Function.get(2, 2, 2, arr));
+    }
+}
